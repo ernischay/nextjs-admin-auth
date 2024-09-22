d9d716f0bc489401921152065d373e2499ce6b5b
ernischay <er.nischay@gmail.com>
2024-09-22 17:53:59 +0530

    update

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -9,14 +9,17 @@ import { AnimatePresence, motion } from 'framer-motion'
 export default function Sidebar() {
     const [width, setWidth] = useState<number>(0)
     const [isOpen, setIsOpen] = useState<boolean>(false)
+    const [isMobile, setIsMobile] = useState<boolean>(false)
 
     const router = useRouter()
 
     const pathname = usePathname()
 
     useEffect(() => {
+        setIsMobile(window.innerWidth < 768)
         setWidth(window.innerWidth)
         const handleResize = () => {
+            setIsMobile(window.innerWidth < 768)
             setWidth(window.innerWidth)
         }
 
@@ -46,6 +49,27 @@ export default function Sidebar() {
         }
     };
 
+    const navVariants = {
+        hidden: {
+            opacity: 0,
+            transition: {
+                staggerChildren: 0.1, // Adds stagger for child elements during exit
+                staggerDirection: -1, // Reverse the direction on exit
+            },
+        },
+        visible: {
+            opacity: 1,
+            transition: {
+                staggerChildren: 0.1,
+            },
+        },
+    };
+
+    const itemVariants = {
+        hidden: { opacity: 0, y: -20 },
+        visible: { opacity: 1, y: 0 },
+    };
+
     return (
         <div className='flex items-center flex-[0.2] flex-col text-white h-screen bg-[#18181B] text-[24px]'>
             <div className="flex w-full justify-center items-center">
@@ -62,13 +86,68 @@ export default function Sidebar() {
             </div>
             <AnimatePresence>
                 {showSidebar && (
-                    <motion.aside
-                        className="w-full top-0 left-0 bg-background"
-                        initial={{ height: 0 }}  // Start with height 0
-                        animate={{ height: isOpen ? 'auto' : 0 }} // Animate to auto when open, back to 0 when closed
-                        exit={{ height: 0 }}
-                        transition={{ duration: 0.3 }}  // Smooth transition
-                    >
+                    isMobile ?
+                        <motion.aside
+                            className="w-full top-0 left-0 bg-background"
+                            initial={{ height: 0 }}  // Start with height 0
+                            animate={{ height: isOpen ? 'auto' : 0 }} // Animate to auto when open, back to 0 when closed
+                            exit={{ height: 0 }}
+                            transition={{ duration: 0.8 }}  // Smooth transition
+                        >
+                            <motion.ul initial="hidden"
+                                animate="visible"
+                                exit="hidden"
+                                transition={{ duration: 0.8 }}
+                                variants={navVariants} className='w-full px-8 mt-[10px] pb-8 flex flex-col gap-y-2'>
+                                <motion.li
+                                    variants={itemVariants}
+                                    className={
+                                        pathname === '/dashboard'
+                                            ? 'text-base cursor-pointer text-primary px-[25px] py-[10px] bg-[white] hover:bg-[white] font-medium transition-colors hover:text-primary'
+                                            : 'flex text-base items-center cursor-pointer px-[25px] py-[10px] hover:bg-[white] hover:text-primary'
+                                    }
+                                    onClick={() => {
+                                        router.refresh()
+                                        router.push('/dashboard')
+                                    }}
+                                >
+                                    <p className='flex ml-[15px] items-center'>Dashboard</p>
+                                </motion.li>
+                                <motion.li
+                                    variants={itemVariants}
+                                    className={
+                                        pathname === '/projects'
+                                            ? 'text-base cursor-pointer text-primary px-[25px] py-[10px] bg-[white] hover:bg-[white] font-medium transition-colors hover:text-primary'
+                                            : 'flex text-base items-center cursor-pointer px-[25px] py-[10px] hover:bg-[white] hover:text-primary'
+                                    }
+                                    onClick={() => {
+                                        router.refresh()
+                                        router.push('/projects')
+                                    }}
+                                >
+                                    <p className='flex ml-[15px] items-center'>Projects</p>
+                                </motion.li>
+                                <motion.li
+                                    variants={itemVariants}
+                                    className={
+                                        pathname === '/settings'
+                                            ? 'text-base cursor-pointer text-primary px-[25px] py-[10px] bg-[white] hover:bg-[white] font-medium transition-colors hover:text-primary'
+                                            : 'flex text-base items-center cursor-pointer px-[25px] py-[10px] hover:bg-[white] hover:text-primary'
+                                    }
+                                    onClick={() => {
+                                        router.refresh()
+                                        router.push('/settings')
+                                    }}
+                                >
+                                    <p className='flex ml-[15px] items-center'>Settings</p>
+                                </motion.li>
+                                <motion.li variants={itemVariants} className='flex text-base items-center cursor-pointer px-[25px] py-[10px] md:bottom-[48px] md:absolute' onClick={() => handleLogout()}>
+                                    <LogOutIcon fill='#fff' width='24px' height='24px' />
+                                    <p className='flex ml-[15px] items-center'>Logout</p>
+                                </motion.li>
+                            </motion.ul>
+                        </motion.aside>
+                        :
                         <ul className='w-full px-8 mt-[10px] pb-8 flex flex-col gap-y-2'>
                             <li
                                 className={
@@ -114,7 +193,6 @@ export default function Sidebar() {
                                 <p className='flex ml-[15px] items-center'>Logout</p>
                             </li>
                         </ul>
-                    </motion.aside>
                 )}
             </AnimatePresence>
         </div >
